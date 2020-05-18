0a8562aa68a355531ed55c17bc919e57f85a09ca
danielaSchneider <danielah@live.com>
2020-05-17 22:39:39 -0700

    routes were added

---
+++ src/views/heroes-data.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="heroes-data">
+    <h1>Heroes Data</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  page: {
+    title: "Heroes Data",
+    meta: [{ name: "description", content: "The Heroes Data page." }]
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
